4cba04bd70f754b70b5eafdeeb45d108b55518bf
Sergio Pedri <sergio0694@live.com>
2021-01-25 22:16:20 +0100

    Remove managed objects from types

--- sources/Interop/D3D12MemoryAllocator/src/D3D12MA_MUTEX.cs
+++ sources/Interop/D3D12MemoryAllocator/src/D3D12MA_MUTEX.cs
@@ -1,21 +1,21 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-using System.Runtime.CompilerServices;
-using System.Threading;
+using static TerraFX.Interop.Windows;
 
 namespace TerraFX.Interop
 {
-    internal readonly struct D3D12MA_MUTEX
+    internal unsafe struct D3D12MA_MUTEX
     {
-        public void Lock() { Monitor.Enter(m_Mutex); }
-        public void Unlock() { Monitor.Exit(m_Mutex); }
+        public void Lock() { WaitForSingleObject(m_Mutex, INFINITE); }
+        public void Unlock() { ReleaseMutex(m_Mutex); }
 
         public static void Init(out D3D12MA_MUTEX mutex)
         {
             mutex = default;
-            Unsafe.AsRef(mutex.m_Mutex) = new();
+            mutex.m_Mutex = CreateMutex(null, 0, null);
         }
 
-        private readonly object m_Mutex;
+        [NativeTypeName("std::mutex")]
+        private HANDLE m_Mutex;
     }
 }
--- sources/Interop/D3D12MemoryAllocator/src/D3D12MA_RW_MUTEX.cs
+++ sources/Interop/D3D12MemoryAllocator/src/D3D12MA_RW_MUTEX.cs
@@ -1,23 +1,24 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
 using System.Runtime.CompilerServices;
-using System.Threading;
+using static TerraFX.Interop.Windows;
 
 namespace TerraFX.Interop
 {
-    internal readonly struct D3D12MA_RW_MUTEX
+    internal unsafe struct D3D12MA_RW_MUTEX
     {
-        public void LockRead() { m_Lock.EnterReadLock(); }
-        public void UnlockRead() { m_Lock.ExitReadLock(); }
-        public void LockWrite() { m_Lock.EnterWriteLock(); }
-        public void UnlockWrite() { m_Lock.ExitWriteLock(); }
+        public void LockRead() { AcquireSRWLockShared((RTL_SRWLOCK*)Unsafe.AsPointer(ref m_Lock)); }
+        public void UnlockRead() { ReleaseSRWLockShared((RTL_SRWLOCK*)Unsafe.AsPointer(ref m_Lock)); }
+        public void LockWrite() { AcquireSRWLockExclusive((RTL_SRWLOCK*)Unsafe.AsPointer(ref m_Lock)); }
+        public void UnlockWrite() { ReleaseSRWLockExclusive((RTL_SRWLOCK*)Unsafe.AsPointer(ref m_Lock)); }
 
         public static void Init(out D3D12MA_RW_MUTEX mutex)
         {
             mutex = default;
-            Unsafe.AsRef(mutex.m_Lock) = new();
+            InitializeSRWLock((RTL_SRWLOCK*)Unsafe.AsPointer(ref mutex.m_Lock));
         }
 
-        private readonly ReaderWriterLockSlim m_Lock;
+        [NativeTypeName("SRWLOCK")]
+        private RTL_SRWLOCK m_Lock;
     }
 }
